b74fd4f1722409a7bb12828a4db6e42a715a2b0e
david <insightintosoftware@gmail.com>
2022-12-22 22:48:01 -0800

    Added s3 multipart upload

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from flask import Flask, request
+import boto3
+from boto3.s3.transfer import TransferConfig
 import requests
 import json
 import os
@@ -34,11 +36,34 @@ def upload_file():
         filenames = [filename for filename in os.listdir('./temp_files') if filename.startswith(chunk_prefix)]
         filenames = sorted(filenames)
 
-        with open("./UploadedFiles/"+request.form['filename'], "ab") as myfile:
+        file_path = "./UploadedFiles/" + request.form['filename']
+        with open(file_path, "ab") as myfile:
             for f in filenames:
                 with open('./temp_files/' +f, "rb") as input_file:
                     myfile.write(input_file.read())
                 os.remove('./temp_files/' +f)
+
+            """
+            # If you want to do multipart upload it AWS S3, uncomment this part.
+            # Actually, you don't need to explicitly ask for a multipart upload.
+            # Just call upload_file, and boto3 will automatically use a multipart upload 
+            # if your file size is above a certain threshold (which defaults to 8MB).
+            bucket_name = 'xxx'
+            access_key_id = 'xxx'
+            access_secret = 'xxx'
+
+            awssession = boto3.Session(aws_access_key_id=access_key_id, aws_secret_access_key=access_secret)
+            s3 = awssession.resource('s3')
+            config = TransferConfig(multipart_threshold=1024*25,
+                                    max_concurrency=10,
+                                    multipart_chunksize=1024*25,
+                                    use_threads=True)
+            
+            key = "other/"+request.form['filename']
+            s3.Object(bucket_name, key).upload_file(file_path,
+                                                    ExtraArgs = {'ContentType': 'text/plain'},
+                                                    Config = config)
+            """
 
     return 'success'
 
